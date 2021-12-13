6a32e98713c7394d2da09ea84a1c6418a75677d6
bondelectrical <aleksejbondarenko18@gmail.com>
2021-12-13 10:04:54 +0200

    Created base data News

---
+++ app/src/main/java/nlt/bondarenko/newsapp/data/News.java
@@ -0,0 +1,41 @@
+package nlt.bondarenko.newsapp.data;
+
+public class News {
+
+    private String urlNews;
+    private String description;
+    private String imgNews;
+    private String textNes;
+
+    public String getUrlNews() {
+        return urlNews;
+    }
+
+    public void setUrlNews(String urlNews) {
+        this.urlNews = urlNews;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgNews() {
+        return imgNews;
+    }
+
+    public void setImgNews(String imgNews) {
+        this.imgNews = imgNews;
+    }
+
+    public String getTextNes() {
+        return textNes;
+    }
+
+    public void setTextNes(String textNes) {
+        this.textNes = textNes;
+    }
+}
